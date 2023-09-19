69bec1175ca0b51b1658b9abd46abc44e316f0c3
fishyboi@MSI <fishyboi@MSI>
2023-09-19 09:48:42 -0400

    is favorite

--- index.js
+++ index.js
@@ -28,17 +28,18 @@ const setup = async() => {
         CREATE TABLE pets(
             id SERIAL PRIMARY KEY,
             name VARCHAR(20),
+            is_favorite BOOLEAN
         );
         INSERT INTO pets (name) VALUES ("Pet 1");
         INSERT INTO pets (name) VALUES ("Pet 2");
         INSERT INTO pets (name) VALUES ("Pet 3");
-        INSERT INTO pets (name) VALUES ("Pet 4");
+        INSERT INTO pets (name, is_favorite) VALUES ("Pet 4", true);
         INSERT INTO pets (name) VALUES ("Pet 5");
     `;
     await client.query(SQL);
     console.log("Aminals are hear");
 
-    const port = process.env.PORT || 3000;
+    const port = process.env.PORT || 5500;
     app.listen(port, ()=> {
         console.log(`always listening on port ${port}`);
     });
